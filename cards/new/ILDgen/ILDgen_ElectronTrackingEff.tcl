2fe78b4558364e30a821e399b801dc5b9e7a59d4
Aleksander Filip Zarnecki <zarnecki@fuw.edu.pl>
2020-06-11 20:52:35 +0200

    Implementing new model, work in progress...

---
+++ cards/new/ILDgen/ILDgen_ElectronTrackingEff.tcl
@@ -0,0 +1,7 @@
+
+  # set EfficiencyFormula {efficiency formula as a function of eta and pt}
+
+  # tracking efficiency formula for electrons
+  set EfficiencyFormula {                                                    (pt <= 0.1)   * (0.00) +
+                                           (abs(eta) <= 2.4)               * (pt > 0.1)    * (0.99) +
+                                           (abs(eta) >  2.4)                               * (0.00)}
