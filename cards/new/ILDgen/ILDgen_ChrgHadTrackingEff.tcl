9cfc73b02e7948d584067383e6a644091e9951dd
Aleksander Filip Zarnecki <zarnecki@fuw.edu.pl>
2020-06-15 20:10:29 +0200

    Updating the test version

--- cards/new/ILDgen/ILDgen_ChrgHadTrackingEff.tcl
+++ cards/new/ILDgen/ILDgen_ChrgHadTrackingEff.tcl
@@ -1,6 +1,4 @@
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 # Tracking efficiency as given in the ILD IDR (Figure 8.2)
 #  for high angles (FTD coverage): educated guess
 #  A.F.Zarnecki, June 12, 2020
@@ -19,31 +17,3 @@
                 (abs(eta) > 2.65 && abs(eta) <= 3.00)     *      (pt > 0.3 && pt <= 0.7)   * (0.945) +
                 (abs(eta) > 2.65 && abs(eta) <= 3.00)     *      (pt > 0.7)                * (0.95)  +
                 (abs(eta) > 3.00)                         *      (pt > 0.1)                * (0.00)  }
-=======
-  # add EfficiencyFormula {efficiency formula as a function of eta and pt}
-
-  # tracking efficiency formula for charged hadrons
-  set EfficiencyFormula {                                                    (pt <= 0.1)   * (0.00) +
-                                           (abs(eta) <= 2.4)               * (pt > 0.1)    * (0.99) +
-                                           (abs(eta) >  2.4)                               * (0.00)}
->>>>>>> Implementing new model, work in progress...
-=======
-# Tracking efficiency as given in the ILD IDR (Figure 8.2)
-#  for high angles (FTD coverage): educated guess
-#  A.F.Zarnecki, June 12, 2020
-
-  set EfficiencyFormula {                                                    (pt <= 0.1)   * (0.00)  +
-                                   (abs(eta) <= 1.83)     *      (pt > 0.1 && pt <= 0.2)   * (0.70)  +
-                                   (abs(eta) <= 1.83)     *      (pt > 0.2 && pt <= 0.3)   * (0.93)  +
-                                   (abs(eta) <= 1.83)     *      (pt > 0.3 && pt <= 0.7)   * (0.995) +
-                                   (abs(eta) <= 1.83)     *      (pt > 0.7)                * (1.00)  +
-                (abs(eta) > 1.83 && abs(eta) <= 2.65)     *      (pt > 0.1 && pt <= 0.2)   * (0.697) +
-                (abs(eta) > 1.83 && abs(eta) <= 2.65)     *      (pt > 0.2 && pt <= 0.3)   * (0.925) +
-                (abs(eta) > 1.83 && abs(eta) <= 2.65)     *      (pt > 0.3 && pt <= 0.7)   * (0.99)  +
-                (abs(eta) > 1.83 && abs(eta) <= 2.65)     *      (pt > 0.7)                * (0.995) +
-                (abs(eta) > 2.65 && abs(eta) <= 3.00)     *      (pt > 0.1 && pt <= 0.2)   * (0.665) +
-                (abs(eta) > 2.65 && abs(eta) <= 3.00)     *      (pt > 0.2 && pt <= 0.3)   * (0.884) +
-                (abs(eta) > 2.65 && abs(eta) <= 3.00)     *      (pt > 0.3 && pt <= 0.7)   * (0.945) +
-                (abs(eta) > 2.65 && abs(eta) <= 3.00)     *      (pt > 0.7)                * (0.95)  +
-                (abs(eta) > 3.00)                         *      (pt > 0.1)                * (0.00)  }
->>>>>>> Implementing tracking efficiency, as shown in ILD IDR Figure 8.2
