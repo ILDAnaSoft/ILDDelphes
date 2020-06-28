0fa3c658f1e87c4d10c64af1b2505252f2b39b21
Aleksander Filip Zarnecki <zarnecki@fuw.edu.pl>
2020-06-28 12:11:33 +0200

    Improved description of BeamCal geometry and reconstruction efficiency

--- cards/new/ILDgen/ILDgen_BeamCalEfficiency.tcl
+++ cards/new/ILDgen/ILDgen_BeamCalEfficiency.tcl
@@ -1,4 +1,4 @@
-# Efficiency formula for photons in BeamCal
+# Efficiency formula for photons in Forward BeamCal
 # Based on IDR Figure 8.8a and Moritz Hebermehl PhD Thesis Figure 4.10
 # A.F.Zarnecki,  June 2020
 
@@ -6,24 +6,8 @@
 
 set EfficiencyFormula {
     (energy <= 10 ) *                                                      (0.00) +
-    
-    (energy > 10 && energy <= 20 ) * (abs(eta) > 4.0 && abs(eta) <= 4.6) * (0.60) +
-    (energy > 10 && energy <= 20 ) * (abs(eta) > 4.6 && abs(eta) <= 5.0) * (0.20) +
-    (energy > 10 && energy <= 20 ) * (abs(eta) > 5.0 && abs(eta) <= 5.4) * (0.05) +
-    (energy > 10 && energy <= 20 ) * (abs(eta) > 5.4 && abs(eta) <= 5.8) * (0.00) +
-    
-    (energy > 20 && energy <= 40 ) * (abs(eta) > 4.0 && abs(eta) <= 4.6) * (0.85) +
-    (energy > 20 && energy <= 40 ) * (abs(eta) > 4.6 && abs(eta) <= 5.0) * (0.60) +
-    (energy > 20 && energy <= 40 ) * (abs(eta) > 5.0 && abs(eta) <= 5.4) * (0.10) +
-    (energy > 20 && energy <= 40 ) * (abs(eta) > 5.4 && abs(eta) <= 5.8) * (0.05) +
-
-    (energy > 40 && energy <= 80 ) * (abs(eta) > 4.0 && abs(eta) <= 4.6) * (0.95) +
-    (energy > 40 && energy <= 80 ) * (abs(eta) > 4.6 && abs(eta) <= 5.0) * (0.75) +
-    (energy > 40 && energy <= 80 ) * (abs(eta) > 5.0 && abs(eta) <= 5.4) * (0.40) +
-    (energy > 40 && energy <= 80 ) * (abs(eta) > 5.4 && abs(eta) <= 5.8) * (0.20) +
-    
-    (energy > 80 ) * (abs(eta) > 4.0 && abs(eta) <= 4.6) * (0.98) +
-    (energy > 80 ) * (abs(eta) > 4.6 && abs(eta) <= 5.0) * (0.90) +
-    (energy > 80 ) * (abs(eta) > 5.0 && abs(eta) <= 5.4) * (0.90) +
-    (energy > 80 ) * (abs(eta) > 5.4 && abs(eta) <= 5.8) * (0.80) 
+    (energy > 10 ) * (abs(eta)  > 5.88 )                                 * (0.00) +
+    (energy > 10 ) * (abs(eta) <= 4.25 )                     * (1.0 - 1.0/energy) +
+    (energy > 10 ) * (abs(eta) > 4.25 && abs(eta) <= 5.88)   *
+         (1.- pow(1.-pow(3.6-abs(eta)/1.634,pow(85./energy,1.4)),4.0)) * (1.0 - 1.0/energy) 
   }
--- cards/new/ILDgen/ILDgen_BeamCalR_Binning.tcl
+++ cards/new/ILDgen/ILDgen_BeamCalR_Binning.tcl
@@ -4,14 +4,26 @@
 set pi [expr {acos(-1)}]
     
 # BeamCal eta range 4.0 - 5.8 -> 2.099-0.347 =1.752deg
-# 1.752/20=0.0876
-# 360/0.0876/cosh(4.9) = ~61 => 64 bins
+#
+# Inner part (4.6-5.8):  1.152-0.347 = 0.805 deg
+# 360/0.0973/cosh(5.3) = ~36 => 32 bins (taking key hole into account!!!)
+#
+# Outer part (4.0-4.6):  2.099-1.152 = 0.947 deg
+# 360/0.0973/cosh(4.4) = ~90 => 90 bins
   #Rear part
   set PhiBins {}
-  for {set i -32} {$i <= 32} {incr i} {
-    add PhiBins [expr {$i * $pi/32.0}]
+  for {set i -45} {$i <= 45} {incr i} {
+    add PhiBins [expr {$i * $pi/45.0}]
   }
-  for {set i 0} {$i <= 20} {incr i} {
-    set eta [expr {-5.8 + $i * 1.8/20.0}]
+  for {set i 0} {$i <= 12} {incr i} {
+    set eta [expr {-4.0 - $i * 0.5999/12.0}]
+    add EtaPhiBins $eta $PhiBins
+  }
+  set PhiBins {}
+  for {set i -16} {$i <= 16} {incr i} {
+    add PhiBins [expr {$i * $pi/18.0}]
+  }
+  for {set i 0} {$i <= 8} {incr i} {
+    set eta [expr {-4.6 - $i * 1.2/8.0}]
     add EtaPhiBins $eta $PhiBins
   }
