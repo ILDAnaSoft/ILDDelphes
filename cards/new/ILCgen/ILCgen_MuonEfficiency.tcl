e466b7e1b3a4373366d7b39b9adb33839eb5f9ac
Aleksander Filip Zarnecki <zarnecki@fuw.edu.pl>
2020-07-10 19:39:23 +0200

    Adjust muon identification efficiency

--- cards/new/ILCgen/ILCgen_MuonEfficiency.tcl
+++ cards/new/ILCgen/ILCgen_MuonEfficiency.tcl
@@ -1,10 +1,8 @@
-
-  # set EfficiencyFormula {efficiency formula as a function of eta and pt}
-
 # efficiency formula for muons
 # Identification based on tracking only
 
   set EfficiencyFormula {
-        (energy <= 2 ) * (0.00) +
-        (energy > 2 )  * (0.95)
+        (energy <= 2 )                  * (0.00) +
+        (energy > 2  && energy <= 10 )  * (0.95) +
+        (energy > 10 )                  * (0.97)
   }
