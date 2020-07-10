0e0346cbf2cc8be13ad4886ad6a996d763218c09
Aleksander Filip Zarnecki <zarnecki@fuw.edu.pl>
2020-07-10 19:39:54 +0200

    More realistic tau tagging

--- cards/new/ILCgen/ILCgen_TauTagging.tcl
+++ cards/new/ILCgen/ILCgen_TauTagging.tcl
@@ -1,4 +1,3 @@
-# This is just a place holder for future development
 
   set DeltaR 0.5
 
@@ -6,9 +5,17 @@
 
   set TauEtaMax 4.0
 
-  # add EfficiencyFormula {abs(PDG code)} {efficiency formula as a function of eta and pt}
-
   # default efficiency formula (misidentification rate)
-  add EfficiencyFormula {0} {0.001}
+  add EfficiencyFormula {0} {
+     (abs(eta) <= 3 ) * (energy <=10 ) *  0.00 +
+     (abs(eta) <= 3 ) * (energy  >10 ) *  0.03 +
+     (abs(eta)  > 3 )                  *  0.00 
+  }
+
   # efficiency formula for tau-jets
-  add EfficiencyFormula {15} {0.4}
+  add EfficiencyFormula {15} {
+    (abs(eta) <= 3 ) * (energy <=10 )                *  0.00 +
+    (abs(eta) <= 3 ) * (energy  >10  && energy<=40 ) * (0.40 + 0.01*energy ) +
+    (abs(eta) <= 3 ) * (energy  >40 )                *  0.80 +
+    (abs(eta)  > 3 )                                 *  0.00 
+  }
