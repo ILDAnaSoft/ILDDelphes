973c97072fd6fe27a3d5e2f366529b425d26040c
Aleksander Filip Zarnecki <zarnecki@fuw.edu.pl>
2020-07-11 11:21:31 +0200

    Correction to BeamCal efficiency

--- cards/new/ILCgen/ILCgen_BeamCalEfficiency.tcl
+++ cards/new/ILCgen/ILCgen_BeamCalEfficiency.tcl
@@ -7,7 +7,7 @@
 set EfficiencyFormula {
     (energy <= 10 ) *                                                      (0.00) +
     (energy > 10 ) * (abs(eta)  > 5.88 )                                 * (0.00) +
-    (energy > 10 ) * (abs(eta) <= 4.25 )                     * (1.0 - 1.0/energy) +
+    (energy > 10 ) * (abs(eta) <= 4.25 )                     * (0.98 - 1.0/energy) +
     (energy > 10 ) * (abs(eta) > 4.25 && abs(eta) <= 5.88)   *
-         (1.- pow(1.-pow(3.6-abs(eta)/1.634,pow(85./energy,1.4)),4.0)) * (1.0 - 1.0/energy) 
+         (1.- pow(1.-pow(3.6-abs(eta)/1.634,pow(85./energy,1.4)),4.0)) * (0.98 - 1.0/energy) 
   }
