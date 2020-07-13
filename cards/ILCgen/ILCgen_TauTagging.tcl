9d3f7f530c64b7c60cfcf62996828993d56090b4
Aleksander Filip Zarnecki <zarnecki@fuw.edu.pl>
2020-07-13 17:46:41 +0200

    Implementing new tau tagging efficiencies

--- cards/ILCgen/ILCgen_TauTagging.tcl
+++ cards/ILCgen/ILCgen_TauTagging.tcl
@@ -7,15 +7,18 @@
 
   # default efficiency formula (misidentification rate)
   add EfficiencyFormula {0} {
-     (abs(eta) <= 3 ) * (energy <=10 ) *  0.00 +
-     (abs(eta) <= 3 ) * (energy  >10 ) *  0.03 +
-     (abs(eta)  > 3 )                  *  0.00 
+     (abs(eta) <= 3 ) * (energy <= 15 )                *   0.000 +
+     (abs(eta) <= 3 ) * (energy  > 15 && energy <=27 ) *  (0.055 - 0.0025  * (energy - 15 ) ) +
+     (abs(eta) <= 3 ) * (energy  > 27 && energy <=52 ) *  (0.025 - 0.00028 * (energy - 27 ) ) +
+     (abs(eta) <= 3 ) * (energy  > 52 )                *   0.018 +
+     (abs(eta)  > 3 )                                  *   0.000 
   }
 
   # efficiency formula for tau-jets
   add EfficiencyFormula {15} {
-    (abs(eta) <= 3 ) * (energy <=10 )                *  0.00 +
-    (abs(eta) <= 3 ) * (energy  >10  && energy<=40 ) * (0.40 + 0.01*energy ) +
-    (abs(eta) <= 3 ) * (energy  >40 )                *  0.80 +
-    (abs(eta)  > 3 )                                 *  0.00 
+     (abs(eta) <= 3 ) * (energy <=15 )                 *   0.000 +
+     (abs(eta) <= 3 ) * (energy  > 15 && energy <=27 ) *  (0.080 + 0.0355  * (energy - 15 ) ) +
+     (abs(eta) <= 3 ) * (energy  > 27 && energy <=52 ) *  (0.506 + 0.00436 * (energy - 27 ) ) +
+     (abs(eta) <= 3 ) * (energy  > 52 )                *   0.615 +
+     (abs(eta)  > 3 )                                  *   0.000 
   }
